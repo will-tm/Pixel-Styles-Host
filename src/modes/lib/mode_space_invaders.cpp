a8e2dde0d6ca7cd796116ccefdd1437eabe69bf9
William Markezana <william.markezana@me.com>
2014-03-03 21:06:01 +0100

    mode Space Invaders added.

---
+++ src/modes/lib/mode_space_invaders.cpp
@@ -0,0 +1,100 @@
+/*
+ * mode_space_invaders.cpp
+ *
+ * Copyright (C) 2014 William Markezana <william.markezana@me.com>
+ *
+ */
+
+#include "mode_space_invaders.h"
+
+#define ANIMATE_DELAY					(500000) // us
+#define INVADERS_WIDTH 					(11)
+#define INVADERS_HEIGHT					(8)
+#define INVADERS_COUNT					(2)
+
+const uint8_t mode_space_invaders::pixels[] =
+{
+	0,0,1,0,0,0,0,0,1,0,0,
+	0,0,0,1,0,0,0,1,0,0,0,
+	0,0,1,1,1,1,1,1,1,0,0,
+	0,1,1,0,1,1,1,0,1,1,0,
+	1,1,1,1,1,1,1,1,1,1,1,
+	1,0,1,1,1,1,1,1,1,0,1,
+	1,0,1,0,0,0,0,0,1,0,1,
+	0,0,0,1,1,0,1,1,0,0,0,
+
+	0,0,1,0,0,0,0,0,1,0,0,
+	1,0,0,1,0,0,0,1,0,0,1,
+	1,0,1,1,1,1,1,1,1,0,1,
+	1,1,1,0,1,1,1,0,1,1,1,
+	1,1,1,1,1,1,1,1,1,1,1,
+	0,1,1,1,1,1,1,1,1,1,0,
+	0,0,1,0,0,0,0,0,1,0,0,
+	0,1,0,0,0,0,0,0,0,1,0
+};
+
+/*
+ * public library interface
+ *
+ */
+extern "C" mode_interface* create_mode(size_t pWidth, size_t pHeight, bool pAudioAvailable)
+{
+  return new mode_space_invaders(pWidth, pHeight, "Space Invaders", pAudioAvailable);
+}
+
+extern "C" void destroy_mode(mode_interface* object)
+{
+  delete object;
+}
+
+/*
+ * constructor
+ *
+ */
+mode_space_invaders::mode_space_invaders(size_t pWidth, size_t pHeight, string pName, bool pAudioAvailable)
+		: mode_interface(pWidth, pHeight, pName, pAudioAvailable)
+{
+	mLastTick = 0;
+	mImageState = 0;
+	
+	// Settings
+	mSettings.add("Color", "Invader", rgb_to_string(ColorGreen), 0.0f, 4294967295.0f, ihmColorSelector);
+	mSettings.set_ini_path(mIniFilePath);
+}
+
+/*
+ * destructor
+ *
+ */
+mode_space_invaders::~mode_space_invaders()
+{
+	
+}
+
+/*
+ * public functions
+ *
+ */
+void mode_space_invaders::paint()
+{
+	rgb_color color = int_to_rgb(mSettings["Color"]->get_value<uint32_t>());
+	int xOffset = (mWidth - INVADERS_WIDTH) / 2;
+	int yOffset = (mHeight - INVADERS_HEIGHT) / 2;
+
+	size_t imageOffset = INVADERS_WIDTH * INVADERS_HEIGHT * mImageState;
+	for (size_t y = 0; y < INVADERS_HEIGHT; y++)
+	{
+		size_t rowOffset = y * INVADERS_WIDTH;
+		for (size_t x = 0; x < INVADERS_WIDTH; x++)
+		{
+			mBitmap->set_pixel(xOffset + x, yOffset + y, pixels[x + rowOffset + imageOffset] == 1 ? color : ColorBlack);
+		}
+	}
+
+	if (get_tick_us() - mLastTick > ANIMATE_DELAY)
+	{
+		mLastTick = get_tick_us();
+		if (++mImageState >= INVADERS_COUNT)
+			mImageState = 0;
+	}
+}
---
+++ src/modes/lib/mode_space_invaders.h
@@ -0,0 +1,30 @@
+/*
+ * mode_space_invaders.h
+ *
+ * Copyright (C) 2014 William Markezana <william.markezana@me.com>
+ *
+ */
+
+#pragma once
+
+using namespace std;
+
+#include "mode_interface.h"
+
+/*
+ * public class
+ *
+ */
+class mode_space_invaders: public mode_interface
+{
+private:
+	uint32_t mLastTick;
+	uint8_t mImageState;
+public:
+	static uint8_t const pixels[];
+
+	mode_space_invaders(size_t pWidth, size_t pHeight, string pName, bool pAudioAvailable);
+	~mode_space_invaders();
+
+	virtual void paint();
+};
